d80fdd498cf36c99bf00c38538efb50fd4158097
pjhun2 <netwo115@naver.com>
2022-01-06 06:09:42 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,11 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  bucket_prefix  = "pjhun"
+module "s3-bucket" {
+  source  = "app.terraform.io/example-org-df4662/s3-bucket/aws"
+  version = "2.2.0"
+  # insert required variables here
+ bucket = "my-s3-bucket"
   acl    = "private"
-
+  bucket_prefix  = pjhun
   versioning = {
     enabled = true
   }
-
 }
